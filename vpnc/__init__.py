c266d52b5335a8eb36085e92eb305100062ec051
Rafael Sagastume <arcangelsagastume@gmail.com>
2019-11-11 13:00:20 -0600

    support python 3

--- vpnc/__init__.py
+++ vpnc/__init__.py
@@ -24,15 +24,16 @@ import sys
 import os
 import subprocess
 from contextlib import contextmanager
+from __future__ import print_function
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
 class VPNC(object):
 
-    def __init__(self, config=dict(),
+    def __init__(self, config=None,
                        config_file="tempvpnc.conf",
                        config_folder=None):
-        self.config = config
+        self.config = config or dict()
         self.config_file = config_file
         self.temp_config_path = os.path.join(HERE, self.config_file)
         self.config_folder = config_folder
@@ -46,22 +47,21 @@ class VPNC(object):
     def create_config_file(self):
         """Creates a formatted VPNC config file."""
         with open(self.temp_config_path, "w+") as f:
-            print >> f, (
-                "IPSec gateway %(IPSec_gateway)s\n"
+            print("IPSec gateway %(IPSec_gateway)s\n"
                 "IPSec ID %(IPSec_ID)s\n"
                 "IPSec secret %(IPSec_secret)s\n"
                 "IKE Authmode %(IKE_Authmode)s\n"
                 "Xauth username %(Xauth_username)s\n"
-                "Xauth password %(Xauth_password)s"
-            ) % self.config
+                "Xauth password %(Xauth_password)s" % self.config,
+                file=f)
 
     def move_config_file(self):
         """Moves the VPNC config file to /etc/vpnc (Linux) or
         /usr/local/etc/vpnc/ (OSX).
         """
-        subprocess.check_call(["sudo", "mv", self.temp_config_path, self.config_folder])
-        subprocess.check_call(["sudo", "chown", "root:root", self.config_path])
-        subprocess.check_call(["sudo", "chmod", "600", self.config_path])
+        subprocess.check_call(["mv", self.temp_config_path, self.config_folder])
+        subprocess.check_call(["chown", "root:root", self.config_path])
+        subprocess.check_call(["chmod", "600", self.config_path])
 
     def remove_config_file(self):
         """Removes the auto-generated VPNC config file."""
@@ -75,11 +75,11 @@ class VPNC(object):
         """Connects to VPNC."""
         self.create_config_file()
         self.move_config_file()
-        subprocess.check_call(["sudo", "vpnc", "tempvpnc"], env=os.environ)
+        subprocess.check_call(["vpnc", "tempvpnc"], env=os.environ)
 
     def disconnect(self):
         """Disconnects from VPNC."""
-        subprocess.call(["sudo", "vpnc-disconnect"])
+        subprocess.call(["vpnc-disconnect"])
         self.remove_config_file()
 
     @contextmanager
